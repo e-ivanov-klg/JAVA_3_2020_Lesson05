43a54bedb044bdf6b75bac95e8416049af3b946c
Evgeny Ivanov <ei.kaluga@gmail.com>
2020-04-09 23:14:44 +0300

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test    {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++){
+            int w = i;
+            System.out.println(w);
+        }
+    }
+}
